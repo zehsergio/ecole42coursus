c282aa1ed6730d207bc467f23927fc1ca838301f
José Dias Junior <josediasjunior@MacBook-Pro-de-Jose-2.local>
2025-10-29 15:59:11 +0100

    norminette adjusts

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdias-ju <jdias-ju@student.42lausanne.ch>    +#+  +:+       +#+      */
+/*   By: jdias-ju <jdias-ju@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/08 20:06:41 by jdias-ju          #+#    #+#             */
-/*   Updated: 2025/10/08 20:54:37 by jdias-ju         ###   ########.ch       */
+/*   Created: 2025/10/29 15:47:29 by jdias-ju          #+#    #+#             */
+/*   Updated: 2025/10/29 15:51:08 by jdias-ju         ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // A function that sets each byte of a memory area to zero,
 // based on the size (number of bytes) passed.
+// char	*p; Pointer used to walk through the memory, byte by byte.
+// p = (char *)s; Copy the memory address from 's' and tell the compiler to treat it as a char pointer (cast).
 
 #include "libft.h"
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	char *p; // Pointer used to walk through the memory, byte by byte.
-	size_t i;
+	char	*p;
+	size_t	i;
 
 	i = 0;
-	p = (char *)s; // Copy the memory address from 's' and tell the compiler to treat it as a char pointer (cast).
+	p = (char *)s;
 	while (i < n)
 	{
 		p[i] = 0;
